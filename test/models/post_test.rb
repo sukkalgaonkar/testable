6faa9393fc04d372d41ac3578d793ab75a9bf72d
Sourabh U <sourabh.ukkalgaonkar@bacancy.com>
2023-11-30 12:29:50 +0530

    Test Pass

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -18,10 +18,4 @@ class PostTest < ActiveSupport::TestCase
 
     assert post.save, 'Saved'
   end
-
-  test 'post should have views as integer' do
-    post = Post.new(title: 'Test', views: 'asdf')
-
-    assert_not post.save, 'Saved the post with a string for'
-  end
 end
